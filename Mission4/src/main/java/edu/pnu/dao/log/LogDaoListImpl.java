4a4d60d8fadae4be51abb9b14f568a7b6a700f49
dan-mi <dan-mi@kakao.com>
2022-11-26 19:31:26 +0900

    LogDaoFileImpl으로 파일명변경

--- Mission4/src/main/java/edu/pnu/dao/log/LogDaoListImpl.java
+++
@@ -1,9 +0,0 @@
-package edu.pnu.dao.log;
-
-public class LogDaoListImpl implements LogDAO {
-
-	@Override
-	public void addLog(String method, String query, Long exedate, boolean success) {
-		
-	}
-}
