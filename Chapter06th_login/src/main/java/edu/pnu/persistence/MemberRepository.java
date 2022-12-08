d143e3917eedff4efc19bf636477b75cb4d6b0db
danmiee <dan-mi@kakao.com>
2022-12-08 11:34:29 +0900

    설명주석 추가

--- Chapter06th_login/src/main/java/edu/pnu/persistence/MemberRepository.java
+++ Chapter06th_login/src/main/java/edu/pnu/persistence/MemberRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.pnu.domain.Member;
 
+// JPA 기능 지원하는 Repository
 public interface MemberRepository extends JpaRepository<Member, String> {
 
 }
--- Chapter06th_login/src/main/java/edu/pnu/service/MemberServiceImpl.java
+++ Chapter06th_login/src/main/java/edu/pnu/service/MemberServiceImpl.java
@@ -16,7 +16,9 @@ public class MemberServiceImpl implements MemberService {
 	
 	public Member getMember(Member member) {
 		Optional<Member> findMember = memberRepo.findById(member.getId());
+		// Optional.isPresent : 객체가 값을 가지고 있다면 true, 없다면 false 리턴
 		if(findMember.isPresent())
+			// 매개변수로 들어온 member의 id가 세션에 저장되어 있으면 그 값을 리턴
 			return findMember.get();
 		else return null;
 	}
