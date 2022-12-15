3af7aae9c399e6c3a879eb9b66177f84c57d44cc
danmiee <dan-mi@kakao.com>
2022-12-15 18:48:23 +0900

    Spring Boot 과제
    	- 2.1.5v에서 작성된 코드 4.0v으로 수정

---
+++ BoardWeb/src/main/java/com/rubypaper/board/domain/Search.java
@@ -0,0 +1,21 @@
+package com.rubypaper.board.domain;
+
+import lombok.Data;
+
+@Data
+public class Search {
+	private String searchCondition;
+	private String searchKeyword;
+	public String getSearchCondition() {
+		return searchCondition;
+	}
+	public void setSearchCondition(String searchCondition) {
+		this.searchCondition = searchCondition;
+	}
+	public String getSearchKeyword() {
+		return searchKeyword;
+	}
+	public void setSearchKeyword(String searchKeyword) {
+		this.searchKeyword = searchKeyword;
+	}
+}
