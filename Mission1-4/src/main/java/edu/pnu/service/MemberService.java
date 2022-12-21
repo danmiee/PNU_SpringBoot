9e4a2c05fed5a0e6e3a650f2bf4abfc419950b43
danmiee <dan-mi@kakao.com>
2022-12-21 19:04:48 +0900

    리스트에서 데이터 서비스
      - 정리 내용 참고하며 작성하기
      - 기존 작성본 참조 X

---
+++ Mission1-4/src/main/java/edu/pnu/service/MemberService.java
@@ -0,0 +1,66 @@
+package edu.pnu.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import edu.pnu.dao.MemberVO;
+
+@Service
+public class MemberService {
+	
+	List<MemberVO> list = new ArrayList<>();
+	
+	public MemberService() {
+		for(int i=1; i<=10; i++) {
+			list.add(new MemberVO(i, "pass"+i, "name"+i, new Date()));
+		}
+	}
+	
+	public List<MemberVO> getMembers(){
+		return list;
+	}
+	
+	public MemberVO getMember(Integer id) {
+		for(int i=0; i<list.size(); i++) {
+			// 인덱스와 id 불일치 주의
+			if(i+1==id)
+				return list.get(i);
+		}
+		return null;
+	}
+	
+	public MemberVO addMember(MemberVO vo) {
+		int id = list.size()+1;
+		vo.setId(id);
+		vo.setRegidate(new Date());
+		list.add(vo);
+		return vo;
+	}
+	
+	public MemberVO updateMember(MemberVO vo) {
+		for(MemberVO m : list) {
+			if(m.getId()==vo.getId()) {
+				m.setName(vo.getName());
+				m.setPass(vo.getPass());
+				return m;
+			}
+		}
+		return null;
+	}
+	
+	public MemberVO removeMember(Integer id) {
+		for(int i=0; i<list.size(); i++) {
+			// 인덱스와 id 불일치 주의
+			if(i+1==id) {
+				MemberVO m = list.get(i);
+				list.remove(i);
+				return m;
+			}
+		}
+		return null;
+	}
+	
+}
