f14b2be4d49710f5477e788c1186fad9d6ebb918
danmiee <dan-mi@kakao.com>
2022-12-27 21:27:44 +0900

    H2 데이터베이스에서 데이터 서비스
      - 정리 내용 보지 않고 작성하기
    
      - 참고한 내용
        - getNextId() query문
        - updateMember() query문

---
+++ Mission2-5/src/main/java/edu/pnu/Mission25Application.java
@@ -0,0 +1,13 @@
+package edu.pnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mission25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mission25Application.class, args);
+	}
+
+}
---
+++ Mission2-5/src/main/java/edu/pnu/service/MemberService.java
@@ -0,0 +1,34 @@
+package edu.pnu.service;
+
+import java.util.List;
+
+import edu.pnu.dao.MemberDao;
+import edu.pnu.domain.MemberVO;
+
+public class MemberService {
+
+	MemberDao dao;
+	public MemberService() {
+		dao = new MemberDao();
+	}
+	
+	public List<MemberVO> getMembers() {
+		return dao.getMembers();
+	}
+	
+	public MemberVO getMember(int id) {
+		return dao.getMember(id);
+	}
+	
+	public MemberVO addMember(MemberVO vo) {
+		return dao.addMember(vo);
+	}
+	
+	public MemberVO updateMember(MemberVO vo) {
+		return dao.updateMember(vo);
+	}
+	
+	public MemberVO removeMember(int id) {
+		return dao.removeMember(id);
+	}
+}
