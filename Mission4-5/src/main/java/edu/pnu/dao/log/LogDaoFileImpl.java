8bce336e5fb2ad0e1bd8258525671eb21bdac95b
danmiee <dan-mi@kakao.com>
2023-01-08 17:10:51 +0900

    LogDaoFileImpl 구현

--- Mission4-5/src/main/java/edu/pnu/dao/log/LogDaoFileImpl.java
+++ Mission4-5/src/main/java/edu/pnu/dao/log/LogDaoFileImpl.java
@@ -1,10 +1,25 @@
 package edu.pnu.dao.log;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class LogDaoFileImpl implements LogInterface {
 
 	@Override
 	public void addlog(String method, String sqlstring, boolean success) {
-		
+
+		try {
+			// 파일열기
+			File f = new File("log.csv");
+			// 파일쓰기(append모드 true)
+			FileWriter fw = new FileWriter(f, true);
+			// 기록내용
+			fw.write(method + "," + sqlstring + "," + success + "\n");
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
